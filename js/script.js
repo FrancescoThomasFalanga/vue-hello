dcf9abac3953cd0bb6770714acfe6c11e0328c26
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-13 12:54:44 +0100

    complete

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      titleText: "Cane Coca-Cola",
+      titleClass: "title",
+      bgImage: "img/cane-cocacola.jpeg",
+      imgClass: "img-size",
+      btnClass: "btn-style",
     }
-  }
+  },
+
+
+  methods: {
+    changeImg() {
+
+        if(this.bgImage == "img/cane-cocacola.jpeg") {
+
+            this.bgImage = "img/cane-cocacola1.jpg";
+
+        } else {
+
+            this.bgImage = "img/cane-cocacola.jpeg";
+
+        }
+
+    }
+  },
 }).mount('#app')
